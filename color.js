2f562c763d1ab4ed1521cfc7a1ce6562a560e6bb
jieunlee <liz950827@gmail.com>
2022-05-15 12:03:36 +0900

    merge all

---
+++ color.js
@@ -0,0 +1,7 @@
+let colorObj = {
+  colorCr: 'rgba(255, 254, 249, 1)',
+  colorYl: 'rgba(237, 235, 178, 1)',
+  colorBp: 'rgba(200, 181, 200, 1)',
+  colorDp: 'rgba(128, 104, 173, 1)',
+  colorGr: 'rgba(196, 196, 196, 1)'
+};
