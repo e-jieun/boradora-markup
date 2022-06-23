d5c335460676253461882b9178df84b3326df519
jieunlee <liz950827@gmail.com>
2022-06-23 16:00:31 +0900

    진한 보라색 컬러 추가됨

--- mobile-tablet/js/module/color.js
+++ mobile-tablet/js/module/color.js
@@ -3,7 +3,8 @@ const colorObj = {
   colorYl: 'rgba(237, 235, 178, 1)',
   colorBp: 'rgba(200, 181, 200, 1)',
   colorDp: 'rgba(128, 104, 173, 1)',
-  colorGr: 'rgba(196, 196, 196, 1)'
+  colorGr: 'rgba(196, 196, 196, 1)',
+  colorGp: 'rgba(100,50,138,1.0)'
 };
 
 export default colorObj;
