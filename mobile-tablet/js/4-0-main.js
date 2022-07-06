8c6b791910a60fe3479152206bb712c047be3b89
jieunlee <liz950827@gmail.com>
2022-07-06 15:32:20 +0900

    양쪽으로 움직이는 슬라이드는 작동, 문제는 이미지 끝 부분에서 멈춰야 하는데 멈춰지지 않는다는 것이다

--- mobile-tablet/js/4-0-main.js
+++ mobile-tablet/js/4-0-main.js
@@ -4,7 +4,8 @@ import {
   dramaTitle
 } from "./module/dramainform.js";
 
-if(window.innerWidth < 1024){
+// *모바일, 태블릿
+if (window.innerWidth < 1024) {
   // *
   document.body.setAttribute('style', `margin: 0; padding: 0; box-sizing: border-box; overflow: hidden;`);
   // *select root
@@ -20,7 +21,7 @@ if(window.innerWidth < 1024){
   console.log(headerLogo);
   // *headerLogo parent style
   headerCon.children[0].setAttribute('style', `width: 50px; height: 50px; display: flex; align-items: center;`);
-  
+
   // *select headerCon.children[1]
   // todo: 클릭 이벤트를 만들어주기, 클릭 이벤트를 만들어서 드롭다운 메뉴로 구성해주기
   // todo: 그리고 클릭 이벤트 타겟이 headerSearch가 아니라면 드롭다운 접어주기 shrink up
@@ -30,7 +31,7 @@ if(window.innerWidth < 1024){
   headerSearch.setAttribute('style', `width: 40px; height: 40px; background: none; border: none; display: flex; align-items: center; justify-content: center;`);
   console.log(headerSearch.firstElementChild);
   headerSearch.firstElementChild.setAttribute('style', `width: 30px; height: 30px; display: flex;`)
-  
+
   // *클릭 이벤트 클릭하면 생겨나는 부분
   // *화면을 덮는 검색창 top: 헤더 바로 밑에 위치하도록 하기; right: 0; 최대 크기 정해줘야 함, 태블릿 버전 때문에 
   console.log(headerSearch.children);
@@ -53,33 +54,33 @@ if(window.innerWidth < 1024){
       `
   searchBar.innerHTML = searchBarText;
   searchBar.setAttribute('style', `width: 100vw; height: 90vmax; position: absolute; top: 10vh; right: 0; background: ${colorObj.colorYl}; z-index: 2;`);
-  
+
   // *생성된 드롭다운 메뉴 전체 컨테이너 부분
   const searchBarCon = headerSearch.firstElementChild.nextElementSibling.firstElementChild;
   console.log(searchBarCon);
   searchBarCon.setAttribute('style', `height: inherit; display: flex; align-items: center; justify-content: flex-start; flex-direction: column;`)
-  
+
   // *검색창
   const searchInput = searchBarCon.firstElementChild;
   console.log(searchInput);
   // https://developer.mozilla.org/ko/docs/Web/CSS/box-shadow
   searchInput.setAttribute('style', `width: 60vw; height: 5vh; border: none; border-radius: 50px; margin-bottom: 2rem;`);
-  
+
   // *추천하는 키워드
   // todo: padding-left 없애줘야 함
   const recommendText = searchInput.nextElementSibling;
   console.log(recommendText);
   recommendText.style.paddingLeft = 0;
   recommendText.style.color = colorObj.colorDp;
-  
+
   // * 태그 부분 => list
   // todo: padding-left 없애줘야 함
   const recommendKeywordCon = searchBarCon.lastElementChild;
   console.log(recommendKeywordCon);
   recommendKeywordCon.setAttribute('style', `height: 50%; display: grid; row-gap: 3vh; padding-left: 0;`);
   recommendKeywordCon.style.height = '50%';
-  
-  
+
+
   // *키워드 아이템
   const recommendKeywordItem = Array.from(recommendKeywordCon.children);
   // console.log(recommendKeywordItem);
@@ -89,7 +90,7 @@ if(window.innerWidth < 1024){
   })
   // todo: 가장 나중에 숨겨줄 부분
   searchBar.style.display = 'none';
-  
+
   let isClick = true;
   headerSearch.firstElementChild.addEventListener('click', (event) => {
     console.log(event.target);
@@ -105,7 +106,7 @@ if(window.innerWidth < 1024){
       isClick = true;
     }
   })
-  
+
   // *select #main
   const mainCon = document.getElementById('main');
   mainCon.setAttribute('style', `width: 100vw; display: grid; grid-template-rows: 4fr 2fr 1fr; align-items: center; justify-content: center; overflow: hidden;`);
@@ -127,14 +128,14 @@ if(window.innerWidth < 1024){
   const tagCon = textCon.children[2];
   // *textCon.children style
   title.setAttribute('style', `font-size: 2rem; color: ${colorObj.colorYl};`);
-  
+
   drama.setAttribute('style', `width: 50vw; text-align: center; color: ${colorObj.colorBp}; text-stroke: 1px`);
   tagCon.setAttribute('style', `display: flex; justify-content: space-around; list-style-type: none;`);
-  
+
   tagCon.style.display = 'flex';
   tagCon.style.justifyContent = 'space-around';
   tagCon.style.listStyleType = 'none';
-  
+
   // *tagCon.children
   console.log(tagCon.children); //arr
   for (let i = 0; i < tagCon.children.length; i++) {
@@ -166,7 +167,7 @@ if(window.innerWidth < 1024){
   // 1. 슬라이드 컨테이너를 배경에 위치시킨다 0
   // 2. 슬라이드 아이템을 배열이니까 컨테이너의 0 자식요소로 불러온다
   // 3. 이미지로 들어갈 배열을 선언(우선 색깔을 넣어주자) 0
-  // 4. 한 섹션마다 이미지를 넣어준다 0
+  // 4. 한 섹션마다 이미지를 넣어준다 0 
   // 5. 클릭이벤트를 달아준다
   // 6. 클릭하면 위치가 이동하는 부분을 주면 좋을 것
   const slideCon = document.getElementById('slide-con');
@@ -183,21 +184,22 @@ if(window.innerWidth < 1024){
   for (let i = 0; i < slideChild.length; i++) {
     slideChild[i].setAttribute('style', `width: 100vw; height: 100vh; min-width: 100vw;`);
     slideChild[i].innerHTML = `<img src=${colorArr[i]}>`;
-  
+
     const slideChildImg = slideChild[i].firstElementChild;
     console.log(slideChildImg);
     slideChildImg.setAttribute('style', `width: 100vw; height: 100vh; object-fit: cover; filter: brightness(0.5);`)
     // slideChildImg.style.objectFit = 'cover';
   }
   // console.log(buttonCon.children);
+  console.clear();
   // *select buttonCon.children[2]
-  const leftBtn = buttonCon.lastElementChild;
-  const rightBtn = buttonCon.firstElementChild;
-  const playBtn = rightBtn.nextElementSibling;
-  console.log(leftBtn);
-  console.log(rightBtn);
+  const rightBtn = buttonCon.lastElementChild;
+  const leftBtn = buttonCon.firstElementChild;
+  const playBtn = leftBtn.nextElementSibling;
+  // console.log('left'+leftBtn);
+  // console.log('right'+rightBtn);
   console.log(playBtn);
-  
+
   // *buttonCon.children
   const buttonItem = buttonCon.children;
   // *buttonCon.children style
@@ -205,14 +207,14 @@ if(window.innerWidth < 1024){
     buttonItem[i].style.background = 'none';
     buttonItem[i].style.border = 'none';
   }
-  
+
   // *playBtn style
   console.log(playBtn);
   playBtn.setAttribute('style', `width: 50px; height: 50px; background: ${colorObj.colorBp}; border: none; transform: rotate(180deg); border-radius: 50%;`);
   // *playBtn.children[0].style.
-  
+
   // *leftBtn style
-  leftBtn.style.transform = 'rotate(180deg)';
+  rightBtn.style.transform = 'rotate(180deg)';
   let widthValue = window.innerWidth;
   let clickValue = 0;
   // 이 부분은 왼쪽 버튼
@@ -220,54 +222,63 @@ if(window.innerWidth < 1024){
   // todo: 이미지 바뀌는 부분에 따라서 텍스트 내보내주기
   title.textContent = dramaTitle[clickValue];
   drama.textContent = dramaExplain[clickValue];
-  
-  leftBtn.addEventListener('click', () => {
-    // console.log('왼쪽으로 이전 이미지가 넘어가는 인터랙션을 주시오');
-    console.dir(slideChild[0]); //offsetWidth: 531;
-    // 넓이 기준점을 0-클릭수만큼 곱해주고
-    // widthValue = slideChild[0].offsetWidth * index;
-    title.textContent = dramaTitle[clickValue];
-    drama.textContent = dramaExplain[clickValue];
-    widthValue = window.innerWidth * clickValue;
-    clickValue++;
-    // 우선 슬라이드 효과는 줄 수 있다
-    for (let i = 0; i < slideChild.length; i++) {
-      // console.log(widthValue);
-      slideChild[i].style.position = 'relative';
-      slideChild[i].style.left = `-${widthValue}px`;
-      // todo: 이미지 바뀌는 부분에 따라서 텍스트 내보내주기
-      // slideChild[i].style.transition = '2s';
-      console.log('현재 left 값 위치: ' + slideChild[i].style.left);
-    }
-    console.log('현재 widthValue: ' + widthValue);
-  
-    let rightClickValue = 1;
-    // todo: 이곳에서 left 값이 얼마인지 알아야 한다
-    rightBtn.addEventListener('click', () => {
-      console.log('rightbtn');
-      rightClickValue++;
-      console.log('현재 오른쪽 버튼 값이 계산되기 전 widthValue: ' + widthValue);
-      for (let i = 0; i < slideChild.length; i++) {
-        // todo: 1. 다시 돌아가는 버튼은 left 버튼의 현재 left 값에서 이동할 수 있어야 한다 
-        // ?한번 클릭했을 때만 되는 문제 해결하기 => 변수를 선언해서 해결했다
-        // ?그런데 left 값이 실행되고 나서의 값이 
-        slideChild[i].style.left = -widthValue + (window.innerWidth * rightClickValue) + 'px';
-        // todo: 2. 그러면 오른쪽 버튼은 left값에 +브라우저 내부값 하나만 더 해주면 되는 것 아닐까? 클릭할 때마다
-        // todo: 3. 그러면 left 값은 -windthValue px에 +window.innerHeight을 더해준 값이 된다
-        // title.textContent = dramaTitle[i];
-        // drama.textContent = dramaExplain[i];
-        console.log('오른쪽 클릭 left값 위치: ' + slideChild[i].style.left);
-      }
-    });
-  });
-  
-  
-  // todo: 다시 돌아가는 버튼(버튼컨의 첫 칠드런)에 이벤트를 누르면 다시 앞의 컨텐츠로 돌아가는 효과를 줘야 한다 
-  // rightBtn.addEventListener('click', () => {
-  //   console.log('rightbtn');
-  //   // todo: 1. 다시 돌아가는 버튼은 left 버튼의 현재 left 값에서 이동할 수 있어야 한다 
-  //   // todo: 2. 그러면 오른쪽 버튼은 left값에 +브라우저 내부값 하나만 더 해주면 되는 것 아닐까? 클릭할 때마다
-  //   // todo: 3. 그러면 left 값은 -windthValue px에 +window.innerHeight을 더해준 값이 된다
-  // });
 
+  // todo: 이제 양옆으로는 잘 움직인다 문제는 텍스트가 제대로 들어가지 않는 것 -> 이 부분은 window.innerwidth로 지금의 left값을 나눠주고 양수로 바꿔주면 인덱스와 같아져서 그 인덱스를 적용시키면 되지 않을까?
+
+  slideCon.style.left = 0;
+  // *rightBtn callback func
+  const rightSlide = function () {
+    let isStatus = slideCon.style.left;
+    console.log(isStatus);
+    isStatus = stringNum(isStatus);
+    // let regex = /[^0-9]/g;
+    // isStatus = parseInt(isStatus.replace(regex,''));
+    // console.log(isStatus);
+
+    let moveToRight = -isStatus-window.innerWidth;
+    console.log(moveToRight);
+    console.log(window.innerWidth);
+    slideCon.style.left = moveToRight+'px';
+    if(moveToRight <= -window.innerHeight*4){
+      console.log('오른쪽 버튼에 의해 움직여진 값은?'+moveToRight);
+      moveToRight = -1860;
+      console.log(moveToRight);
+    }
+    // ?얘는
+    // widthValue >= slideCon.style.width ? widthValue = slideCon.style.width : '';
+    console.log('현재 widthValue: ' + widthValue);
+  }
+
+  // *leftBtn callback func
+  const leftSlide = function() {
+    console.log(slideCon.style.left);
+    // *slideCon의 left의 현재값
+    let isStatus = slideCon.style.left;
+    // let regex = /[^0-9]/g;
+    // ?변경된 문자열을 새로 반환해주는 문자열 메서드 .replace()
+    // ?regex부분에 사용한 정규식: /[^0-9]/g === 0-9의 정수가 아닌 것을 제외하고 전체에서 검사해서 반환
+    // ?현재 값을 음수가 아닌 양수로 구해준 부분
+    isStatus = stringNum(isStatus);
+    // isStatus = parseInt(isStatus.replace(regex, ''));
+    // console.log(isStatus);
+
+    // ?-1000+500
+    let moveToLeft = -isStatus + window.innerWidth;
+    console.log(moveToLeft);
+
+    slideCon.style.left = moveToLeft + 'px';
+    console.log(slideCon.style.left);
+    // *left 값이 0보다 크면 0에서 멈춰주도록 한 부분
+    // ?왜 다시 흰 화면으로 갔다가 돌아오는 거지....
+    moveToLeft >= -window.innerWidth && moveToLeft < 0 ? moveToLeft = 0 : '';
+    // todo: 글씨도 함께 변하도록 해줘야 한다
+  }
+
+  // *문자열에서 숫자만 들어간 문자열을 다시 정수로 되돌려주도록 하는 함수 
+  function stringNum(elem){
+    let regex = /[^0-9]/g;
+    return elem = parseInt(elem.replace(regex,''));
+  }
+  rightBtn.addEventListener('click', rightSlide);
+  leftBtn.addEventListener('click', leftSlide);
 }
